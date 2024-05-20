c79d3cdaca630b3ff8863ae87f5f8871e7054dc9
Jake Park <jake.park2398@gmail.com>
2024-05-19 22:43:05 -0600

    Fix statusbar color

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { DarkTheme, NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import StackNavigator from './app/screens/StackNavigator';
+import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 
 import { CustomDarkTheme } from './app/data/themes';
 
 export default function App() {
   return (
     <NavigationContainer theme={CustomDarkTheme}>
+      <StatusBar/>
       <StackNavigator />
     </NavigationContainer>
   );
